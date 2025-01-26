f3fc110343f87b43b93d0c5279a48c89a5b9e5ee
Werner Strydom <bloudraak@users.noreply.github.com>
2025-01-26 07:13:21 -0800

    Create main.ml

---
+++ main.ml
@@ -0,0 +1,6 @@
+(* Hello World in OCaml *)
+(* OCaml uses static typing and type inference *)
+(* The print_endline function returns unit type - similar to void *)
+(* OCaml programs are expressions that evaluate to values *)
+
+print_endline "Hello, World!"
